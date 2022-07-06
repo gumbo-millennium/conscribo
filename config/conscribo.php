1a2df2ff860e591df61b81b5fec60682de822855
Roelof Roos <github@roelof.io>
2022-07-06 16:09:56 +0200

    Update config

--- config/conscribo.php
+++ config/conscribo.php
@@ -1,5 +1,32 @@
 <?php
-// config for Gumbo/Conscribo
-return [
 
+declare(strict_types=1);
+
+return [
+    'auth' => [
+        'account' => env('CONSCRIBO_ACCOUNT'),
+        'username' => env('CONSCRIBO_USERNAME'),
+        'passphrase' => env('CONSCRIBO_PASSPHRASE'),
+    ],
+
+    'objects' => [
+        'user' => [
+            'model' => App\User::class,
+            'resource' => 'persoon',
+            'fields' => [
+                'id' => 'conscribo_id',
+                // further fields to auto-apply to the user model
+            ],
+        ],
+
+        'group' => [
+            'model' => App\Group::class,
+            'resource' => 'orgaan',
+            'fields' => [
+                'id' => 'conscribo_id',
+                'naam' => 'name',
+                // further fields to auto-apply to the group model
+            ],
+        ],
+    ],
 ];
